e68da1fa4cdc17d39b4bc3b358f189b9c1ab2a8a
Shrivarshan <srivarshan0015@gmail.com>
2026-03-28 15:09:32 +0530

    test

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -41,7 +41,7 @@ function Home() {
             hover:animate-[gradientMove_4s_ease_infinite]
           "
         >
-          Shrivarshan 
+          Shrivarshan chinniah 
         </motion.h1>
 
         {/* ROLE */}
